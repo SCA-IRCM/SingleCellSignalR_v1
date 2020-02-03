664d230afb69b150375d693ca8c9ba68b9bdb004
SCA-IRCM <52911489+SCA-IRCM@users.noreply.github.com>
2020-02-03 10:12:45 +0100

    Update

--- SingleCellSignalR/R/PwC_ReactomeKEGG.R
+++ SingleCellSignalR/R/PwC_ReactomeKEGG.R
@@ -1,12 +1,10 @@
 #' Pathway Commons Reactome KEGG 2019-05-08
 #'
-#' @format A data frame with 26067 rows of 6 variables:
+#' @format A data frame with 343230 rows of 4 variables:
 #' \describe{
 #'   \item{a.gn}{interactant 1 gene symbol}
 #'   \item{b.gn}{interactant 2 gene symbol}
 #'   \item{type}{interaction type}
-#'   \item{source}{Reactome and/or KEGG}
-#'   \item{pmid}{PubMed ID}
 #'   \item{pathway}{Associated pathway}
 #'   ...
 #' }
